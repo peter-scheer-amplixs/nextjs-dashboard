b96a2b4407ba3c034b16c2c3795e790eab7c29b6
peter.scheer <peter.scheer@amplixs.com>
2024-12-10 16:42:53 +0100

    Chapter 6

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,7 +3,7 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
-    <div
+  <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
